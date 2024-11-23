46a08202c24dedfd973b79d2778b4f104a464b23
Alex Krol <kromail@list.ru>
2024-11-23 23:52:47 +0600

    fix specs titles

--- lesson-2/foobar_spec.rb
+++ lesson-2/foobar_spec.rb
@@ -4,12 +4,12 @@ require 'rspec'
 require_relative 'foobar_defs'
 
 describe 'foobar' do
-  it 'должен возвращать другое число, если одно из чисел равно 20' do
+  it 'должно возвращать другое число, если одно из чисел равно 20' do
     expect(foobar_out(20, 30)).to eq 30
     expect(foobar_out(40, 20)).to eq 40
   end
 
-  it 'должен возвращать сумму чисел, если первое не равно 20' do
+  it 'должно возвращать сумму чисел, если первое не равно 20' do
     expect(foobar_out(10, 30)).to eq 40
   end
 end
